dbd054f1b594d06c4b31ebbec38860e51016a45c
karri mounika <Karrimounika92@gmail.com>
2024-11-27 11:30:27 +0530

    Commited Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
-Class Test
+public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world !!")
+        System.out.println("Hello world !!");
     }
 }
